7b5c7438247057c78c52b7d85a1c5cc95c9b85a4
Tomosuke Okada <orenoti@gmail.com>
2017-09-05 20:30:43 +0900

    Add basic files.

---
+++ DemoApplication/DemoApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoApplication
+//
+//  Created by Tomosuke Okada on 2017/09/05.
+//  Copyright © 2017年 TomosukeOkada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftyGantt/SwiftyGantt.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyGantt.h
+//  SwiftyGantt
+//
+//  Created by Tomosuke Okada on 2017/09/05.
+//  Copyright © 2017年 TomosukeOkada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyGantt.
+FOUNDATION_EXPORT double SwiftyGanttVersionNumber;
+
+//! Project version string for SwiftyGantt.
+FOUNDATION_EXPORT const unsigned char SwiftyGanttVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyGantt/PublicHeader.h>
+
+
