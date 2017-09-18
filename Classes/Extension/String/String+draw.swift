12e903fd4068ff29baa93c1f983f250a86ae5633
Tomosuke Okada <orenoti@gmail.com>
2017-09-19 00:01:18 +0900

    Add Extensions.

---
+++ Classes/Extension/String/String+draw.swift
@@ -0,0 +1,9 @@
+//
+//  String+draw.swift
+//  SwiftyGantt
+//
+//  Created by Tomosuke Okada on 2017/09/18.
+//  Copyright © 2017年 TomosukeOkada. All rights reserved.
+//
+
+import Foundation
---
+++ Classes/Extension/UIBezierPath/UIBezierPath+figure.swift
@@ -0,0 +1,9 @@
+//
+//  UIBezierPath+figure.swift
+//  SwiftyGantt
+//
+//  Created by Tomosuke Okada on 2017/09/18.
+//  Copyright © 2017年 TomosukeOkada. All rights reserved.
+//
+
+import Foundation
